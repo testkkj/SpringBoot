b4d99ef1aef999a96c34bc331343a768590b041d
testkkj <46573845+testkkj@users.noreply.github.com>
2021-01-01 23:12:17 +0900

    jap 적용중

--- board/src/main/java/com/example/board/BoardApplication.java
+++ board/src/main/java/com/example/board/BoardApplication.java
@@ -2,8 +2,13 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.data.convert.Jsr310Converters;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@EntityScan(basePackageClasses = {Jsr310Converters.class}, basePackages = {"board"})
 @SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
 public class BoardApplication {
 
--- board/src/main/java/com/example/board/configuration/DatabaseConfiguration.java
+++ board/src/main/java/com/example/board/configuration/DatabaseConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.board.configuration;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -13,6 +15,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 /**
@@ -56,5 +59,11 @@ public class DatabaseConfiguration {
     @ConfigurationProperties(prefix = "mybatis.configuration")
     public org.apache.ibatis.session.Configuration mybatisConfig() {
         return new org.apache.ibatis.session.Configuration();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.jpa")
+    public Properties hibernateConfig() {
+        return new Properties();
     }
 }
