6c4f338d40f811fa3f6e179c0ee62af4f76b5b52
testkkj <46573845+testkkj@users.noreply.github.com>
2021-01-02 23:48:50 +0900

    JPA Entity 작성

---
+++ board/src/main/java/com/example/board/board/entity/BoardEntity.java
@@ -0,0 +1,55 @@
+package com.example.board.board.entity;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * BoardEntity
+ */
+@Entity
+@Table(name = "t_jpa_board")
+@NoArgsConstructor
+@Data
+public class BoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int boardIdx;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private int hitCnt = 0;
+
+    @Column(nullable = false)
+    private String creatorId;
+
+    @Column(nullable = false)
+    private LocalDateTime createdDatetime = LocalDateTime.now();
+
+    private String updaterId;
+
+    private LocalDateTime updateDatetime;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "board_idx")
+    private Collection<BoardFileEntity> fileList;
+
+
+
+    
+}
